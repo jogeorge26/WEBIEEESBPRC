4b8ebf971699b6208d380dfaeb7f1d88b3f02930
Anand Sudeep <anandsudeep5@gmail.com>
2024-08-15 11:21:22 +0530

    home bg image correction

--- app/page.js
+++ app/page.js
@@ -2,29 +2,33 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="pt-0">
       <section
-      className="relative flex items-center justify-start h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url(/home.png)", height: "90vh" }}
-    >
-      <div className="absolute inset-0"></div>
-      <div>
-        <div className="relative z-10 text-left text-black px-4 md:px-8">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">
-            IEEE STUDENT BRANCH
-          </h1>
-          <p className="text-lg md:text-xl mb-8">
-            Providence College of Engineering
-          </p>
-          <a
-            href="#explore"
-            className="inline-block px-6 py-3 text-red-500 font-semibold rounded-lg border-2 border-red-500 hover:bg-red-500 transition-colors hover:text-white"
-          >
-            Explore Now
-          </a>
+        className="relative flex items-center justify-start h-screen bg-contain bg-no-repeat bg-right-bottom"
+        style={{
+          backgroundImage: "url(/home.png)",
+          // height: "100vh",
+          // width: "100vw",
+        }}
+      >
+        <div className="absolute inset-0"></div>
+        <div>
+          <div className="relative z-10 text-left text-black px-4 md:px-8">
+            <h1 className="text-4xl md:text-6xl font-bold mb-4">
+              IEEE STUDENT BRANCH
+            </h1>
+            <p className="text-lg md:text-xl mb-8">
+              Providence College of Engineering
+            </p>
+            <a
+              href="#explore"
+              className="inline-block px-6 py-3 text-red-500 font-semibold rounded-lg border-2 border-red-500 hover:bg-red-500 transition-colors hover:text-white"
+            >
+              Explore Now
+            </a>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </main>
   );
 }
